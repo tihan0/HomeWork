cac7c5771ab64b20177e3f0333bfd1dede079a3f
tihan0 <alexttmsc55@gmail.com>
2022-02-09 21:40:35 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,52 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        /*
+        double a = 2.0;
+        double b = 3.0;
+        double c = 4;
+        int d = 5;
+        int e = 10;
+        double f = 11;
+        double g = 15;
+
+        double h = ((d + e) / g) * f;
+
+        double j = ((e + e) / (a * b)) + a;
+
+        double k = b * (b + c);
+        double l = (c * c) / (d);
+        double z = (k / l) - (a);
+
+
+        System.out.println(h);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(z);*/
+
+/*
+        int a = 5;
+        if (a > 5) {
+            System.out.println("число больше 5");
+        } else {
+        System.out.println("число не больше 5");
+    }
+        System.out.println("программа завершена");
+}*/
+
+        int a = 6;
+/*        if (a < 0) {
+            System.out.println("число отрицательное");
+        } else if (a == 0) {
+            System.out.println("число равняется 0");
+        } else {
+        System.out.println("число положительное");
+        }
+        System.out.println("программа завершена");*/
+
+
+        System.out.println( a>6 ? ",больше 5" : " меньше либо равно 5");
+
+        }
+}
